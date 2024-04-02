945252e200bda820de4335efb53b45ccd7a812eb
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-04-02 20:35:32 +0100

    hello wrold🤐🙃

---
+++ defined.js
@@ -0,0 +1,28 @@
+// todo: add more things and cry
+let defined = [
+    "l",
+    "print",
+    "f",
+    "for",
+    "if",
+    "else",
+    "do",
+    "AY",
+    "+",
+    "-",
+    "/",
+    "*",
+    "++",
+    "--",
+    "try",
+    "catch",
+    "timer",
+    "read",
+    "write",
+    "rand",
+    "round",
+    "floor",
+    "interval",
+    "Day"
+]
+// todo : yee when i think of somethings to include
--- out.js
+++ out.js
@@ -2,25 +2,9 @@
  const {print, timer, Day, interval, read, write, appendFile, dirname} = require('./utils')
  const {AY} = require(__dirname +'/objects/AY');
   try {
-let a = "my program variables are nicely scoped" ; 
-let b = "hello world" ; 
-let c = 3 + 3 ; 
-let d = round(rand() * 12) ; 
-function print5(){ ; 
-for ( let i = 0; i < 5; i++ ) { 
-let b2 = round(rand() * 5) ; 
-if ( d > c ) { ; 
-console.log(b)   ; 
+for ( let i = 0; i < 100000; i++ ) { 
+console.log(i)   ; 
 } 
-print(b2) ; 
-} 
-} 
-timer(print5, 500) ; 
-print5() ; 
-let today = new Day() ; 
-let time = today.time() ; 
-print("hey, world!", d, b, c, a, time, today.getFullDate(), PI(), AY.type(AY.isAy), AY.argv[1]) ; 
-; 
 }catch(e){
  console.error(e.message);
 }
